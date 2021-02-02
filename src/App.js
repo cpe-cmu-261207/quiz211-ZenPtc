7b7d59a7452dd341cd5cbe0f0b44f6c10153901c
TK_PTC <50984892+ZenPtc@users.noreply.github.com>
2021-02-02 13:42:37 +0700

    one

--- src/App.js
+++ src/App.js
@@ -1,3 +1,24 @@
+const Table = ({ name, gender, age }) => {
+  return (
+    <>
+      <table class="table is-bordered mb-3">
+        <tbody>
+          <tr>
+            <th>Name</th>
+            <th>Gender</th>
+            <th>Age</th>
+          </tr>
+          <tr>
+            <td>{name}</td>
+            <td>{gender}</td>
+            <td>{age}</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
+};
+
 function App() {
   const persons = [
     {
@@ -9,27 +30,20 @@ function App() {
       name: "Alice",
       gender: "male",
       age: "20"
+    },
+    {
+      name: "Naruporn Powthongchin 620610794",
+      gender: "female",
+      age: "20"
     }
   ];
   return (
     <div class="ml-2">
       <h3 class="title is-3">Person List</h3>
-
       {/* Convert me to a component! */}
-      <table class="table is-bordered mb-3">
-        <tbody>
-          <tr>
-            <th>Name</th>
-            <th>Gender</th>
-            <th>Age</th>
-          </tr>
-          <tr>
-            <td>Bob</td>
-            <td>male</td>
-            <td>50</td>
-          </tr>
-        </tbody>
-      </table>
+      {persons.map((person) => (
+        <Table name={person.name} gender={person.gender} age={person.age} />
+      ))}
 
       {/* Code me please! */}
       <h3 class="title is-3">ID Counter</h3>
